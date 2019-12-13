ca76db06dd7ab99ce9116674a4cf9b52853d0e9e
Braydon Nelson <braydon.nelson@outlook.com>
2019-12-12 18:50:31 -0700

    made it show only one question at a time

--- questions.js
+++ questions.js
@@ -1,22 +1,22 @@
 var questionArray = [
     {
-        question: "What is 1 + 1?",
+        title: "What is 1 + 1?",
         choices: ["2", "4", "7", "8"],
         answer: "2"
 
     },
     {
-        question: "What is 2 + 2?",
+        title: "What is 2 + 2?",
         choices: ["4", "6", "8", "1"],
         answer: "4"
     },
     {
-        question: "What is 4 + 4?",
+        title: "What is 4 + 4?",
         choices: ["8", "5", "2", "1"],
         answer: "8"
     },
     {
-        question: "What is 8 + 8?",
+        title: "What is 8 + 8?",
         choices: ["16", "3", "24", "23"],
         answer: "16"
     }
--- script.js
+++ script.js
@@ -2,44 +2,125 @@ $(document).ready(function () {
     var start = document.querySelector("#start");
     var timer = document.querySelector("#timer");
     var spot = document.querySelector("#spot");
+    var countDown;
+    var timeLeft = 0;
+    var totalScore = 100;
+
+    for (let i = 0; i < questionArray.length; i++) {
+        let num = Math.floor(Math.random() * i);
+        let temp = questionArray[i];
+        questionArray[i] = questionArray[num];
+        questionArray[num] = temp;
+    }
 
     $(start).on("click", function () {
-        for (var i = 0; i < questionArray.length; i++) {
-            var question = questionArray[i].question;
-            var choices = questionArray[i].choices;
-            var answer = questionArray[i].answer;
-            console.log(question);
-            console.log(choices);
-            console.log(answer);
+        timerCount();
+        nextQuestion();
 
+        var retry = document.createElement("button");
+        retry.setAttribute("type", "button");
+        retry.setAttribute("class", "btn btn-primary btn-lg ml-4");
+        retry.innerHTML = "Retry";
+        spot.append(retry);
 
+        $(retry).on("click", function () {
+            window.location.reload();
+        });
 
-            var questionArea = document.createElement("h3");
-            questionArea.setAttribute("class", "question-area")
-            questionArea.innerHTML = question;
-            spot.appendChild(questionArea);
+    });
 
-            for (let i = 0; i < choices.length; i++) {
-                var option = document.createElement("button");
-                var br = document.createElement("br");
-                option.setAttribute("type", "button")
-                option.setAttribute("class", "btn option-btn btn-secondary btn-lg mb-3")
-                option.setAttribute("data-number", i)
-                option.innerHTML = choices[i];
-                spot.append(option);
-                spot.append(br);
+    function submitQuiz() {
+        clearTimeout(countDown);
+        $(spot).empty();
+        var scoreSpot = document.createElement("h1");
+        scoreSpot.setAttribute("class", "display-3");
+        var name = prompt("Please input your name");
+        scoreSpot.innerHTML = name + " - Score - " + totalScore;
+        spot.append(scoreSpot);
+    }
+    function timerCount() {
+        timeLeft = 100;
+        var counter = timer;
 
+        countDown = setInterval(countdown, 1000);
+
+        function countdown() {
+            if (timeLeft < 1) {
+                clearTimeout(countDown);
+                alert("TIMES UP!!!");
+                submitQuiz();
+            } else {
+                counter.innerHTML = timeLeft + ' seconds remaining';
+                console.log(timeleft);
+                timeLeft--;
+                totalScore--;
             }
-
-            $("button").on("click", function () {
-                if (this.classList.contains("option-btn")) {
-                    if (this.innerHTML === questionArray.answer[i]) {
-                        console.log("TRUE BOI");
-                    }
-                }
-            });
-
         }
 
-    })
+    }
+    var i = 0;
+    function nextQuestion() {
+
+        var question = questionArray[i].title;
+        var choices = questionArray[i].choices;
+        var answer = questionArray[i].answer;
+        console.log(question);
+        console.log(choices);
+        console.log(answer);
+
+        for (let i = 0; i < choices.length; i++) {
+            let num = Math.floor(Math.random() * i);
+            let temp = choices[i];
+            choices[i] = choices[num];
+            choices[num] = temp;
+        }
+
+        var questionArea = document.createElement("h3");
+        questionArea.setAttribute("class", "question-area");
+        questionArea.innerHTML = question;
+        spot.appendChild(questionArea);
+
+        for (let i = 0; i < choices.length; i++) {
+            var option = document.createElement("button");
+            var br = document.createElement("br");
+            option.setAttribute("type", "button");
+            option.setAttribute("class", "btn option-btn btn-secondary btn-lg mb-3");
+            option.setAttribute("data-answer", answer);
+            option.innerHTML = choices[i];
+            spot.append(option);
+            spot.append(br);
+
+
+
+
+        }
+        i++;
+        $("button").on("click", function () {
+            if (this.classList.contains("option-btn")) {
+                var userChoice = this.textContent;
+
+
+                if (userChoice === this.dataset.answer) {
+                    console.log("CORRECT!!");
+                    this.setAttribute("style", "background-color: green");
+
+                } else if (userChoice != this.dataset.answer) {
+                    this.setAttribute("style", "background-color: red");
+                    console.log("liaushdfiouashfgiouysdhbfiguyhsdioruyfhosiurf")
+                    timeLeft - 20;
+                    console.log(timeLeft);
+                    totalScore - 20;
+                    console.log(totalScore);
+                }
+                $(spot).empty();
+                if (i != questionArray.length) {
+                    nextQuestion();
+                } else {
+                    submitQuiz();
+                }
+
+            }
+        });
+    }
+
 });
